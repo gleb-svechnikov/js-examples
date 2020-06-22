7e538cee5926fdf63de528415f975441299bcab0
Gleb Svechnikov <gleb.svechnikov@simplinic.com>
2020-06-22 09:36:35 +0300

    Update formula

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ function getGBasedonLocation() {
       console.log(position.coords)
       const height = position.coords.altitude
       const latitude = position.coords.latitude
-      const sin2Φ = Math.sin(latitude)
+      const sin2Φ = Math.sin(2 * latitude)
       const equatorGravity = 9.780327
-      const GRS80_A = 0.0053024
-      const GRS80_B = 0.0000058
-      const IGF = equatorGravity * (1 + GRS80_A * sin2Φ - GRS80_B * sin2Φ)
+      const GRS80_A = 0.0053024 * Math.pow(sin2Φ, 2)
+      const GRS80_B = 0.0000058 * sin2Φ
+      const IGF = equatorGravity * (1 + GRS80_A - GRS80_B)
       const FAC = -3.086e-6 * height
       const localG = IGF + FAC
       console.log(IGF, FAC)
 
-      document.getElementsByTagName('output')[0].append(localG)
+      document.getElementsByTagName('output')[0].textContent = localG
     })
   } else {
     alert('Geolocation is not supported by this browser.')
