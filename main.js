4df6b43ffc96d0ca3317e9f8571bb3a685546454
Gleb Svechnikov <gleb.svechnikov@simplinic.com>
2020-07-03 21:40:42 +0300

    Add simple search

--- main.js
+++ main.js
@@ -19,13 +19,13 @@ function getGBasedonLocation() {
   })
 }
 
-function loadBooks() {
+async function loadBooks() {
   const books = fetch(url)
     .then((response) => {
       return response.json()
     })
     .then((data) => {
-      console.log(data)
+      return data
     })
     .catch((error) => {
       return error
@@ -33,43 +33,89 @@ function loadBooks() {
   console.log(books)
   return books
 }
+let bookList = []
 
-function loadAndShowBooks() {
-  fetch(url)
-    .then(function (response) {
-      return response.json()
-    })
-    .then(function (data) {
-      console.log(data)
-      // document.getElementsByTagName('pre')[0].textContent = JSON.stringify(
-      //   data,
-      //   null,
-      //   2
-      // )
-      const tableReference = document.getElementsByTagName('table')[0]
-      const columns = ['Name', 'Author']
-      let header = document.createElement('thead')
-      header.insertRow()
-      for (let i = 0; i < columns.length; i++) {
-        let headerCell = document.createElement('th')
-        headerCell.align = 'left'
-        headerCell.innerHTML = columns[i]
-        header.appendChild(headerCell)
-      }
-      tableReference.appendChild(header)
-      let body = document.createElement('tbody')
-      data.forEach((book) => {
-        let row = document.createElement('TR')
-        let cell1 = row.insertCell(0)
-        let cell2 = row.insertCell(1)
-        cell1.innerHTML = book.title
-        cell2.innerHTML = book.author
+function showBooks(columns, tableReference, data) {
+  let header = document.createElement('thead')
+  header.insertRow()
+  for (let i = 0; i < columns.length; i++) {
+    let headerCell = document.createElement('th')
+    headerCell.align = 'left'
+    headerCell.innerHTML = columns[i]
+    header.appendChild(headerCell)
+  }
+  tableReference.appendChild(header)
+  let body = document.createElement('tbody')
+  data.forEach((book) => {
+    let row = document.createElement('TR')
+    let cell1 = row.insertCell(0)
+    let cell2 = row.insertCell(1)
+    cell1.innerHTML = book.title
+    cell2.innerHTML = book.author
 
-        body.appendChild(row)
-      })
-      tableReference.appendChild(body)
+    body.appendChild(row)
+  })
+  tableReference.appendChild(body)
+}
+function updateBooks(tableReference, data) {
+  let newBody = document.createElement('tbody')
+  data.forEach((book) => {
+    let row = document.createElement('TR')
+    let cell1 = row.insertCell(0)
+    let cell2 = row.insertCell(1)
+    cell1.innerHTML = book.title
+    cell2.innerHTML = book.author
+
+    newBody.appendChild(row)
+  })
+  tableReference.replaceChild(newBody, tableReference.tBodies[0])
+}
+
+async function loadAndShowBooks() {
+  bookList = await loadBooks()
+  console.log(bookList)
+  const columns = ['Name', 'Author']
+  const tableReference = document.getElementsByTagName('table')[0]
+  if (bookList.length > 0) {
+    showBooks(columns, tableReference, bookList)
+  }
+}
+// function sleep(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms))
+// }
+
+// function infiniteLoop() {
+//   const iceCapsAreMelting = true
+//   let polarBears = 5
+//   sleep(2000).then(() => {
+//     // Инициируем бесконечный цикл
+//     while (iceCapsAreMelting) {
+//       console.log(`There are ${polarBears} polar bears.`)
+//       polarBears--
+
+//       // Выставляем условие выхода из бесконечного цикла
+//       // if (polarBears === 0) {
+//       //   console.log('There are no polar bears left.')
+//       //   break
+//       // }
+//     }
+//   })
+// }
+
+function updateSearch(event) {
+  console.log(event.value)
+  if (event.value.length > 0) {
+    const newList = bookList.filter((book) => {
+      return book.title.includes(event.value)
     })
-    .catch(function (err) {
-      console.log(err)
-    })
+    console.log(newList)
+    const columns = ['Name', 'Author']
+    const tableReference = document.getElementsByTagName('table')[0]
+    if (bookList.length > 0) {
+      updateBooks(tableReference, newList)
+    }
+  } else {
+    const tableReference = document.getElementsByTagName('table')[0]
+    updateBooks(tableReference, bookList)
+  }
 }
